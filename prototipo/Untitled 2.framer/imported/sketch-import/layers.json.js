3a862f483c59b0cb8632ef05668bc782c15d89e1
mariliaferreira <mariliaferreiraa@gmail.com>
2015-04-21 19:21:31 -0300

    Interface App

---
+++ prototipo/Untitled 2.framer/imported/sketch-import/layers.json.js
@@ -0,0 +1,454 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["sketch-import/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "56DB4302-794F-4B5C-9630-C5DD0903F72E",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-okAdd-56DB4302-794F-4B5C-9630-C5DD0903F72E.png",
+          "frame" : {
+            "y" : 78,
+            "x" : 537,
+            "width" : 42,
+            "height" : 29
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 78,
+          "x" : 537,
+          "width" : 42,
+          "height" : 29
+        },
+        "name" : "okAdd"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "C0F7F098-AC6B-4672-8A27-3DB078134075",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-pao-C0F7F098-AC6B-4672-8A27-3DB078134075.png",
+          "frame" : {
+            "y" : 552,
+            "x" : 488,
+            "width" : 82,
+            "height" : 122
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 552,
+          "x" : 488,
+          "width" : 82,
+          "height" : 122
+        },
+        "name" : "pao"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "A05FAE0E-4C9F-44CD-962A-EA03A711600B",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-banana-A05FAE0E-4C9F-44CD-962A-EA03A711600B.png",
+          "frame" : {
+            "y" : 550,
+            "x" : 268,
+            "width" : 114,
+            "height" : 117
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 550,
+          "x" : 268,
+          "width" : 114,
+          "height" : 117
+        },
+        "name" : "banana"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "31358E43-51E1-409C-B645-01196F5B3012",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "1C887B5B-FF0D-4764-B318-3DEA3663A6DC",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-Imported_Layers-1C887B5B-FF0D-4764-B318-3DEA3663A6DC.png",
+              "frame" : {
+                "y" : 526,
+                "x" : 67,
+                "width" : 75,
+                "height" : 95
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 526,
+              "x" : 67,
+              "width" : 75,
+              "height" : 95
+            },
+            "name" : "Imported_Layers"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-cafe-31358E43-51E1-409C-B645-01196F5B3012.png",
+          "frame" : {
+            "y" : 526,
+            "x" : 66,
+            "width" : 76,
+            "height" : 141
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 526,
+          "x" : 66,
+          "width" : 76,
+          "height" : 141
+        },
+        "name" : "cafe"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "358F7A42-511E-4055-8346-F40D09115951",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "8DB9A259-8862-4440-A2F9-C71942FAFBAF",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-Imported_Layers-8DB9A259-8862-4440-A2F9-C71942FAFBAF.png",
+              "frame" : {
+                "y" : 342,
+                "x" : 498,
+                "width" : 93,
+                "height" : 71
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 342,
+              "x" : 498,
+              "width" : 93,
+              "height" : 71
+            },
+            "name" : "Imported_Layers"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-tea-358F7A42-511E-4055-8346-F40D09115951.png",
+          "frame" : {
+            "y" : 342,
+            "x" : 498,
+            "width" : 93,
+            "height" : 115
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 342,
+          "x" : 498,
+          "width" : 93,
+          "height" : 115
+        },
+        "name" : "tea"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "3A695685-0469-4D81-8333-DE6C2F66823C",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-maca-3A695685-0469-4D81-8333-DE6C2F66823C.png",
+          "frame" : {
+            "y" : 332,
+            "x" : 278,
+            "width" : 85,
+            "height" : 132
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 332,
+          "x" : 278,
+          "width" : 85,
+          "height" : 132
+        },
+        "name" : "maca"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "0B700FED-D4F1-411A-B6EE-74ADE8E32CB7",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-sucoSelected-0B700FED-D4F1-411A-B6EE-74ADE8E32CB7.png",
+          "frame" : {
+            "y" : 326,
+            "x" : 57,
+            "width" : 86,
+            "height" : 131
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 326,
+          "x" : 57,
+          "width" : 86,
+          "height" : 131
+        },
+        "name" : "sucoSelected"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "2456068E-B832-4D3F-88CD-9BA3E14746F5",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-voltar-2456068E-B832-4D3F-88CD-9BA3E14746F5.png",
+          "frame" : {
+            "y" : 74,
+            "x" : 57,
+            "width" : 33,
+            "height" : 33
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 74,
+          "x" : 57,
+          "width" : 33,
+          "height" : 33
+        },
+        "name" : "voltar"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "7D2FBBE6-E479-4571-AA78-296B99B6886D",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "children" : [
+              {
+                "maskFrame" : null,
+                "id" : "325DF616-57C1-4D84-B15F-A0008F1E2263",
+                "visible" : true,
+                "children" : [
+
+                ],
+                "image" : {
+                  "path" : "images\/Layer-Status_Bar_Black-325DF616-57C1-4D84-B15F-A0008F1E2263.png",
+                  "frame" : {
+                    "y" : 9,
+                    "x" : 11,
+                    "width" : 618,
+                    "height" : 17
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 9,
+                  "x" : 11,
+                  "width" : 618,
+                  "height" : 17
+                },
+                "name" : "Status_Bar_Black"
+              }
+            ],
+            "id" : "AC4B5335-6714-438E-80D7-77DBFDD9043A",
+            "visible" : true,
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 9,
+              "x" : 11,
+              "width" : 618,
+              "height" : 17
+            },
+            "name" : "statusbar"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-navBar-7D2FBBE6-E479-4571-AA78-296B99B6886D.png",
+          "frame" : {
+            "y" : 0,
+            "x" : 0,
+            "width" : 640,
+            "height" : 141
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 0,
+          "x" : 0,
+          "width" : 640,
+          "height" : 141
+        },
+        "name" : "navBar"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "198A895E-A461-4A63-820D-C4306921F7FC",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-oQueVoce-198A895E-A461-4A63-820D-C4306921F7FC.png",
+          "frame" : {
+            "y" : 202,
+            "x" : 56,
+            "width" : 382,
+            "height" : 36
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 202,
+          "x" : 56,
+          "width" : 382,
+          "height" : 36
+        },
+        "name" : "oQueVoce"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "439EE6E5-AD6B-4E61-AA1F-AA289EDD389E",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "F104C483-5E7F-4904-9218-CBD2E9EF69AE",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-add-F104C483-5E7F-4904-9218-CBD2E9EF69AE.png",
+              "frame" : {
+                "y" : 902,
+                "x" : 510,
+                "width" : 29,
+                "height" : 28
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 902,
+              "x" : 510,
+              "width" : 29,
+              "height" : 28
+            },
+            "name" : "add"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-botaoAdd-439EE6E5-AD6B-4E61-AA1F-AA289EDD389E.png",
+          "frame" : {
+            "y" : 854,
+            "x" : 461,
+            "width" : 126,
+            "height" : 132
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 854,
+          "x" : 461,
+          "width" : 126,
+          "height" : 132
+        },
+        "name" : "botaoAdd"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "63D035C2-BECE-42A7-98EE-CBC1FD897207",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-cuboAcucar-63D035C2-BECE-42A7-98EE-CBC1FD897207.png",
+          "frame" : {
+            "y" : 795,
+            "x" : 79,
+            "width" : 153,
+            "height" : 176
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 795,
+          "x" : 79,
+          "width" : 153,
+          "height" : 176
+        },
+        "name" : "cuboAcucar"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "84D9AD4F-AED5-4A95-9BC5-97C3A10F97D5",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-background-84D9AD4F-AED5-4A95-9BC5-97C3A10F97D5.png",
+          "frame" : {
+            "y" : 0,
+            "x" : 1,
+            "width" : 639,
+            "height" : 1136
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 0,
+          "x" : 1,
+          "width" : 639,
+          "height" : 1136
+        },
+        "name" : "background"
+      }
+    ],
+    "id" : "250E4D8B-2FFF-49C4-BEB2-011153DD6918",
+    "visible" : true,
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 640,
+      "height" : 1136
+    },
+    "name" : "iPhone_55S5C"
+  }
+]
